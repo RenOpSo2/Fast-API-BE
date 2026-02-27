0d5718c5e7b3c24f586283b3e6b68f8dc19e7e61
RenOpSo2 <renzx430@gmail.com>
2026-02-27 01:32:53 +0000

    Update Fix Type Hint

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Response
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Literal
 
-app = FastAPI()
+ENVIRONMENT = os.getenv("ENVIRONMENT", "Development")
+
+class ServerStatus(BaseModel):
+    message: str
+    status: Literal ["Development", "Production", "Staging"]
+    version: str = "0.0.1"
+
+app = FastAPI(
+    title="Fast-API-BE",
+    description="A properly structured FastAPI starter",
+    version="0.0.1"
+)
+
 
 # Basic Route
 @app.get("/")
 async def home_page():
     return {
-    "message": "Server Is Activate",
-    "status": "Development Server"
+    "message": "Server is Activate",
+    "status": ENVIRONMENT
 }
+
 
 @app.get("/help", response_class=HTMLResponse)
 async def help_page():
-    return "<h1>This server was created by the FastAPI library and has been optimized in more depth</h1>"
-
+    return """
+    <html>
+        <head><title>Hell</title></head>
+        <body>
+            <strong>Server Status</strong>
+            <p>This server is built with FastAPI.</p>
+            <p>Check the /docs endpoint for Swagger documentation.</p>
+        </body>
+    </html>
+    """
